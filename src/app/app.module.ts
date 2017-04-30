a144afacf11196ea2e2ad721385f730efde5f87c
rwaida <rwaidaAlmehanni>
2017-04-30 14:51:19 +0300

    working in signup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,21 +2,33 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
+import { LocationStrategy , HashLocationStrategy } from '@angular/common';
+import {RouterModule} from '@angular/router';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { SignupComponent } from './signup/signup.component';
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot([
+  {path:'',component:HomeComponent},
+  { path: 'signup', component: SignupComponent }
+  
+  
+])
   ],
-  providers: [],
+
+  providers: [{provide:LocationStrategy,useClass:HashLocationStrategy}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
