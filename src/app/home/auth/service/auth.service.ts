2c5268b41241d9d21d08e133ccfd8576298a16c1
rwaida <rwaidaAlmehanni>
2017-05-02 17:13:11 +0300

    style the code & add comments

--- src/app/home/auth/service/auth.service.ts
+++ src/app/home/auth/service/auth.service.ts
@@ -1,33 +1,37 @@
 import { Injectable } from '@angular/core';
-import {Http, Headers} from '@angular/http';
+import { Http , Headers } from '@angular/http';
 import 'rxjs/add/operator/map';
 
 @Injectable()
+
 export class AuthService {
 
-  constructor(private http : Http) { }
+  constructor(private http : Http ) { }
 
-    signin(user) {
-    	console.log("xxxxx")
+    signin(user) { //http request when the user sign in ...
+
     let headers = new Headers();
-    headers.append('Content-Type','application/json');
+    headers.append('Content-Type','application/json'); //add the type of data to the header...
     return this.http.post('api/user/signin', user, {headers: headers})
     .map(res => res.json());
+
      }
 
-    storeInLocalStorage(token,id,name){
-      localStorage.setItem('id_token', token);
-      localStorage.setItem('user-id', id);
-      localStorage.setItem('user-name', name);
-    }
+    signup(user) { // http request when the user sign up ...
 
-  signup(user){
   	let headers = new Headers();
-  	headers.append('Content-Type' , 'application/json');
+  	headers.append('Content-Type' , 'application/json'); 
   	return this.http.post('api/user/signup', user , {headers:headers})
   	.map(res => res.json());
 
+     }
 
-  }
+    storeInLocalStorage(token,id,name){ //function to store data in the localStorage ...
+
+      localStorage.setItem('id_token', token); //store the user token in the localStorage ... 
+      localStorage.setItem('user-id', id); //store the user _id in the localStorage ... 
+      localStorage.setItem('user-name', name); //store the user name in the localStorage ... 
+
+    }
 
 }
