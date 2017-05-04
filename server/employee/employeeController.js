3281a07734e6b28d3d298ec98124560f32d044c9
ibrahimtamimi <ibrahimtamimi5@gmail.com>
2017-05-04 11:58:54 +0300

    (fix) api

--- server/employee/employeeController.js
+++ server/employee/employeeController.js
@@ -1,15 +1,13 @@
 const jwt = require('jwt-simple');
 const employeeModel = require('./employeeModel.js');
 const mongoose = require ('mongoose');
-const helper = require('../config/helper.js')
-
 
 module.exports = {
 	addEmp:(req, res)=>{
 		let employeeData  = req.body.employee;
 		employeeModel.findOne({userName : employeeData.userName}, (err, employeeEX)=> {
 			if (employeeEX) {
-				res.json({isemployeeExist : true })
+				res.json({ isemployeeExist : true })
 			}else {
 				employeeModel.create(employeeData, (err, data)=> {
 					if (err) {
@@ -24,11 +22,11 @@ module.exports = {
 
 	signin : (req, res) => {
 		let employeeData = req.body.employee
-		employeeModel.findOne({username : employeeData.userName}, (err, user)=> {
+		employeeModel.findOne({userName : employeeData.userName}, (err, user)=> {
 			if (!user) {
 				res.json({isUser : false});
 			}else{
-				if(user.password === req.body.password){
+				if(user.password === employeeData.password){
 					var token = jwt.encode(user, 'secret');
 					res.setHeader('x-access-token',token);
 					res.json({token: token, id : user._id, userName : user.firstName + " " + user.lastName});
