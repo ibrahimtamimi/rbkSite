e113a5b02575119e16e31210651569360de46cac
rwaida <rwaidaAlmehanni>
2017-05-19 21:46:05 +0300

    make apply to the signup page

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router';
+
 
 @Component({
   selector: 'app-navbar',
@@ -6,10 +8,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
-
-  constructor() { }
+  
+  constructor(private router: Router) { }
 
   ngOnInit() {
+  }
+  callSignup(){
+    this.router.navigate(['/signup']);
   }
 
 }
