222ddeee76c1bc84f4a1cd00fae63458501d9d4c
rwaida <rwaidaAlmehanni>
2017-05-19 23:28:59 +0300

    add shadow to the box and modyfiy styling

--- src/app/footbar/footbar.component.ts
+++ src/app/footbar/footbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-footbar',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FootbarComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
+  }
+  signUp(){
+  	this.router.navigate(['/signup']);
   }
 
 }
