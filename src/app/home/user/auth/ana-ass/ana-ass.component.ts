6429454321a6a67ec94b845d03e6a4e54efe9f49
rwaida <rwaidaAlmehanni>
2017-05-20 23:34:50 +0300

    analytical is ready now

--- src/app/home/user/auth/ana-ass/ana-ass.component.ts
+++ src/app/home/user/auth/ana-ass/ana-ass.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./ana-ass.component.css']
 })
 export class AnaAssComponent implements OnInit {
+	  public qusArr=["do as you would be done by!","As you saw , so will you reap!","life what ever will continue !!!"];
 
   constructor() { }
 
